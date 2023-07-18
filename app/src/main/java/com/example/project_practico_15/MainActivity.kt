8c69afd64a0ff7101629c8d134d6532163f0dd50
Flopy <f.hidalgoalarcon@gmail.com>
2023-07-18 19:33:27 -0400

    Se crea layout item.xml

--- app/src/main/java/com/example/project_practico_15/MainActivity.kt
+++ app/src/main/java/com/example/project_practico_15/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.project_practico_15
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+/* Pasos para crear recycleView
+* 1.[]Item layout
+* 2.[]layout con recyclerView
+* 3.Asignar layout manager
+* 4.[]Adapter + ViewHolder
+* 5.[]Tener disponibles los datos
+* */
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
 }
