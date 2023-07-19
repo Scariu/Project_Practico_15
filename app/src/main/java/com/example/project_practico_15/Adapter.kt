daf192fe544ff5be531a85d011ebba5724dbbd79
Flopy <f.hidalgoalarcon@gmail.com>
2023-07-18 20:27:20 -0400

    Se implementa clase Adapter con sus metodos y class ViewHolder.

--- app/src/main/java/com/example/project_practico_15/Adapter.kt
+++ app/src/main/java/com/example/project_practico_15/Adapter.kt
@@ -1,4 +1,32 @@
 package com.example.project_practico_15
 
-class Adapter {
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+/*
+[x] Implementar clase Adapter
+[x] Heredar RecuclerView
+[x] Creaciones de métodos: onCreateViewHolder, onBindViewHolder y getItemCount
+[x] Crear class ViewHolder
+[x] Heredad RecyclerView.ViewHolder a class ViewHolder
+[x] Agregar constructor
+
+ */
+class Adapter: RecyclerView.Adapter<Adapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Adapter.ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: Adapter.ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+    class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
+
+    }
 }
