fd61b601e028efff9deee298411ed250231d6bba
Flopy <f.hidalgoalarcon@gmail.com>
2023-07-18 20:54:33 -0400

    Se implementa binding en class Adapter.

--- app/src/main/java/com/example/project_practico_15/Adapter.kt
+++ app/src/main/java/com/example/project_practico_15/Adapter.kt
@@ -1,8 +1,10 @@
 package com.example.project_practico_15
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.example.project_practico_15.databinding.ItemBinding
 
 /*
 [x] Implementar clase Adapter
@@ -16,7 +18,8 @@ import androidx.recyclerview.widget.RecyclerView
 class Adapter: RecyclerView.Adapter<Adapter.ViewHolder>() {
 val pokemones = mutableListOf<Pokemon>()
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Adapter.ViewHolder {
-        TODO("Not yet implemented")
+        val binding = ItemBinding.inflate(LayoutInflater.from(parent.context))
+        return ViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: Adapter.ViewHolder, position: Int) {
@@ -27,7 +30,7 @@ val pokemones = mutableListOf<Pokemon>()
         return pokemones.size
 
     }
-    class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
+    class ViewHolder(binding: ItemBinding): RecyclerView.ViewHolder(binding.root){
 
     }
 }
